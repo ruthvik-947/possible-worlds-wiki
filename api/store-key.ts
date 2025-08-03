3fbe4545abe219a34094ed55d04791f977b733d7
ruthvik-947 <ruthvik.ped94@gmail.com>
2025-08-03 17:59:45 +0100

    fix js imports

--- api/store-key.ts
+++ api/store-key.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { activeApiKeys } from './utils/shared';
+import { activeApiKeys } from './utils/shared.js';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method !== 'POST') {
